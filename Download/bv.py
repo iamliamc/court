808bcc3a9b74a98b8b3e694f414d9211325807b2
iamliamc <ljc3179@gmail.com>
2015-07-31 22:36:57 -0400

    Now checks count file for starting value, writes out exception number, breaks on first exception, writes files in site folder

--- Download/bv.py
+++ Download/bv.py
@@ -4,11 +4,10 @@ from __future__ import division
 import csv, os, re, urllib2, urllib, requests, cookielib, mechanize, robotparser, time, random, string, sys, sqlite3, time
 from bs4 import BeautifulSoup
 
-print "We are in the right spot"
-
 #Our source
 gr_url = 'http://grcourt.org/CourtPayments/loadCase.do?caseSequence=1'
-nmax = 3
+nmax = 10000
+
 
 #Get Cookie
 r = requests.get('http://grcourt.org/CourtPayments/loadCase.do?caseSequence=1')
@@ -21,6 +20,8 @@ opener.addheaders.append(('Cookie', headers))
 count_incompletes= open('exceptions.txt', 'w+')
 out_count = open('count.txt', 'r+')
 
+print "We are in the right spot"
+os.chdir("site")
 
 count = out_count.read()
 my_count = int(count)
@@ -42,29 +43,20 @@ while my_count < nmax:
 			# count +=1
 			# print "ZZZZZZZ..."
 			# time.sleep(1.8)
-		if data_ccsort.string == 'Unable to load case data':
-			print "Unable to load case data"
-			my_count += 1
-			time.sleep(1.8)
+		# if data_ccsort.string == 'Unable to load case data':
+			# print "Unable to load case data"
+			# my_count += 1
+			# time.sleep(1.8)
 			#count_incompletes.write(str(count))
-		else:
-			criminal_out = open(str(count) +'.html', 'ab')
-			criminal_out.write(str(bsoup))
-			criminal_out.close()
-			my_count += 1
-			time.sleep(1.8)
+		criminal_out = open(str(my_count) +'.html', 'ab')
+		criminal_out.write(str(bsoup))
+		criminal_out.close()
+		my_count += 1
+		time.sleep(1.8)
 	except:
 		print "################ EXCEPTION #######################"
 		time.sleep(10)
 		count_incompletes.write(str(my_count))
-
-print my_count
-out_count.seek(0)
-out_count.write(str(my_count))
-out_count.truncate()
-out_count.close()
-		
-		
 		
 		#### Do we want to REREQUEST AND CONTINUE? ####
 		# r = requests.get('http://grcourt.org/CourtPayments/loadCase.do?caseSequence=1')
@@ -74,6 +66,12 @@ out_count.close()
 		# opener.addheaders.append(('Cookie', headers))
 		# continue
 
+
+print my_count
+out_count.seek(0)
+out_count.write(str(my_count))
+out_count.truncate()
+out_count.close()
 count_incompletes.close()
 print "Start time:", localtime
 print "End time:", time.localtime(time.time())
